6b8a3c36b12313732355e69453e9d3605071e9a4
kotarella1110 <kotarella1110@gmail.com>
2023-12-21 21:39:14 +0900

    feat: update Waku and Kuma UI

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,15 @@
-"use client";
+'use client';
 
-import { useState } from "react";
+import { useState } from 'react';
+import { Box } from "@kuma-ui/core";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
   return (
-    <div style={{ border: "3px blue dashed", margin: "1em", padding: "1em" }}>
+    <Box border={"3px blue dashed"} m={"1em"} p={"1em"}>
       <p>Count: {count}</p>
       <button onClick={() => setCount((c) => c + 1)}>Increment</button>
       <h3>This is a client component.</h3>
-    </div>
+    </Box>
   );
 };
